93a6baad51dc65b787e286f84ab3ed41eb13f18a
Michael J. Gilliland <mjg.py3@gmail.com>
2014-06-23 21:24:26 -0400

    Started flailing on a deserializer. Got rid of old code, started over. Seems appropriate with a new use case in mind :)

--- go/src/gosoon/gosoon.go
+++ go/src/gosoon/gosoon.go
@@ -1,58 +1,7 @@
 package gosoon
 
-import (
-    "unicode/utf8"
-    "strings"
-)
+type BlueJson struct {}
 
-const (
-    JsonArray = iota
-    JsonNumber
-    JsonObject
-)
-
-type Parser struct { }
-
-func (p *Parser) Parse(jsonText string) JsonNode {
-    if jsonText == "{}" {
-        return NewJsonObject()
-    }
-	return NewJsonArray(jsonText)
-}
-
-type JsonNode struct {
-    _type int
-    _elementCount int
-    NumericValue float64
-}
-
-func NewJsonNumber() JsonNode {
-    jsonNumber := JsonNode { _type: JsonNumber }
-    jsonNumber.NumericValue = 3.0
-    return jsonNumber
-}
-
-func NewJsonObject() JsonNode {
-    return JsonNode { _type: JsonObject }
-}
-
-func NewJsonArray(jsonText string) JsonNode {
-    elementCount := 0
-    innerText := jsonText[1:len(jsonText)-1]
-    if utf8.RuneCountInString(innerText) > 0 {
-        elementCount = len(strings.Split(innerText, ","))
-    }
-    return JsonNode { _type: JsonArray, _elementCount: elementCount }
-}
-
-func (self JsonNode) Child(i int) JsonNode {
-    return NewJsonNumber()
-}
-
-func (self JsonNode) ElementCount() int {
-    return self._elementCount
-}
-
-func (self JsonNode) Type() int {
-    return self._type
+func (self BlueJson) Deserialize(json string, toFill interface{}) interface{} {
+    return toFill
 }
--- go/src/gosoon/gosoon_test.go
+++ go/src/gosoon/gosoon_test.go
@@ -7,62 +7,24 @@ import (
     . "github.com/onsi/gomega"
 )
 
+type TestBag struct {
+    Count int
+}
+
 var _ = Describe("Gosoon", func() {
     var (
-        parser Parser
-        subject JsonNode
+        subject TestBag
     )
 
-    Describe("the parse method", func() {
-        Context("When given an empty JSON array", func() {
-            BeforeEach(func() {
-                subject = parser.Parse("[]")
-            })
-
-            It("Should return a JsonNode with no children", func() {
-                Expect(subject.ElementCount()).To(Equal(0))
-            })
-
-            It("Should return a JsonNode whose type is an Array", func() {
-                Expect(subject.Type()).To(Equal(JsonArray))
-            })
-        })
-
-        Context("When given an nonempty JSON array", func() {
-            Context("And the JSON array contains one element", func() {
-                It("Should return a JsonNode with one child", func() {
-                    Expect(parser.Parse("[3]").ElementCount()).To(Equal(1))
-                })
-
-                Context("And the first element is numeric", func() {
-                    Describe("the first element", func() {
-                        It("Should be numeric", func() {
-                            Expect(parser.Parse("[3]").Child(0).Type()).To(Equal(JsonNumber))
-                        })
-                        It("Should have a numeric value", func() {
-                            Expect(parser.Parse("[3]").Child(0).NumericValue).To(Equal(3.0))
-                        })
-                    })
-                })
-
-                Context("And the JSON array contains two elements", func() {
-                    It("Should return a JsonNode with two elements", func() {
-                        Expect(parser.Parse("[3,5]").ElementCount()).To(Equal(2))
-                    })
-                })
-            })
-
-            Context("When given an empty JSON object", func() {
+    Describe("BlueJson", func() {
+        Describe(".Deserialize", func() {
+            Context("When given an empty JSON array", func() {
                 BeforeEach(func() {
-                    subject = parser.Parse("{}")
+                    subject = (BlueJson{}).Deserialize("{}", TestBag{}).(TestBag)
                 })
 
-                It("Should return a JsonNode whose type is an Object", func() {
-                    Expect(subject.Type()).To(Equal(JsonObject))
-                })
-
-                It("Should return an empty JSON object", func() {
-                    Expect(subject).To(Equal(NewJsonObject()))
+                It("Should have the default value for its field", func() {
+                    Expect(subject.Count).To(Equal(0))
                 })
             })
         })
