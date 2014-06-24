d8653e533f11b7401461090d2ee547c4048c1e0c
Kazark <kazark@zoho.com>
2014-06-24 19:42:29 -0400

    Disabled red spec, ripped out code, added spec
    Gilli! Getting ahead of yourself!

--- go/src/gosoon/gosoon.go
+++ go/src/gosoon/gosoon.go
@@ -1,26 +1,12 @@
 package gosoon
 
 import (
-    "strings"
-    "reflect"
+    _"strings"
+    _"reflect"
 )
 
 type BlueJson struct {}
 
 func (self BlueJson) Deserialize(json string, toFill interface{}) interface{} {
-    withoutCurlies := json[1:len(json)-1]
-
-    fields := strings.FieldsFunc(withoutCurlies, func(r rune) bool {
-        return r == ','
-    })
-
-    for i := range fields {
-        _ = strings.FieldsFunc(fields[i], func(r rune) bool {
-            return r == ','
-        })
-
-        _ = reflect.TypeOf(toFill)
-    }
-
     return toFill
 }
--- go/src/gosoon/gosoon_test.go
+++ go/src/gosoon/gosoon_test.go
@@ -7,19 +7,21 @@ import (
     . "github.com/onsi/gomega"
 )
 
+type Blank struct {}
+
 type TestBag struct {
     Count int
     Phrase string
 }
 
 var _ = Describe("Gosoon", func() {
-    var (
-        subject TestBag
-    )
-
     Describe("BlueJson", func() {
         Describe(".Deserialize", func() {
             Context("When given an empty JSON array", func() {
+                var (
+                    subject TestBag
+                )
+
                 BeforeEach(func() {
                     subject = (BlueJson{}).Deserialize("{}", TestBag{}).(TestBag)
                 })
@@ -33,12 +35,30 @@ var _ = Describe("Gosoon", func() {
                 })
             })
 
+            Context("When given a JSON object, but serializing to an object with no attributes", func() {
+                var (
+                    subject Blank
+                )
+
+                BeforeEach(func() {
+                    subject = (BlueJson{}).Deserialize("{ \"Phrase\": \"a\" }", Blank{}).(Blank)
+                })
+
+                XIt("Should return a blank object", func() {
+                    Expect(subject).To(Equal(Blank{}))
+                })
+            })
+
             Context("When given a JSON object with a string field (1 char), whose attribute matches the databag's", func() {
+                var (
+                    subject TestBag
+                )
+
                 BeforeEach(func() {
                     subject = (BlueJson{}).Deserialize("{ \"Phrase\": \"a\" }", TestBag{}).(TestBag)
                 })
 
-                It("Should have the JSON value for the string field", func() {
+                XIt("Should have the JSON value for the string field", func() {
                     Expect(subject.Phrase).To(Equal("a"))
                 })
             })
