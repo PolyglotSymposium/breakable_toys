e942f47bf8769da7c3cbc5001234ccf3c6ace7b3
Michael J. Gilliland <mjg.py3@gmail.com>
2014-06-24 20:05:32 -0400

    Made the first one pass... Simple, easy case... Next one should be more fun

--- go/src/gosoon/gosoon.go
+++ go/src/gosoon/gosoon.go
@@ -2,11 +2,17 @@ package gosoon
 
 import (
     _"strings"
-    _"reflect"
+    "reflect"
 )
 
-type BlueJson struct {}
+type BlueJson struct {
+    Mappings map[string]reflect.Type
+}
 
 func (self BlueJson) Deserialize(json string, toFill interface{}) interface{} {
     return toFill
+}
+
+func (self *BlueJson) Inspect(me interface{}) {
+    self.Mappings = map[string]reflect.Type{}
 }
