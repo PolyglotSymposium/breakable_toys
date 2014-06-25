d574e4e046d3acd01cf313e932e770a55ba31406
Kazark <kazark@zoho.com>
2014-06-24 21:11:12 -0400

    First step to actually parsing JSON

--- go/src/gosoon/parser.go
+++ go/src/gosoon/parser.go
@@ -1,12 +1,20 @@
 package gosoon
 
+import "strings"
+
 type LazilyParsedJson struct {
+    unparsedJson string
 }
 
 func Json(rawJson string) LazilyParsedJson {
-    return LazilyParsedJson{}
+    return LazilyParsedJson{unparsedJson: rawJson}
 }
 
 func (self LazilyParsedJson) ParseOneLevel() map[string]LazilyParsedJson {
-    return nil
+    parsedMap := make(map[string]LazilyParsedJson)
+    keyvalue := strings.Split(self.unparsedJson, ":")
+    if len(keyvalue) > 1 {
+        parsedMap[keyvalue[0]] = LazilyParsedJson{}
+    }
+    return parsedMap
 }
--- go/src/gosoon/parser_test.go
+++ go/src/gosoon/parser_test.go
@@ -8,13 +8,23 @@ import (
 )
 
 var _ = Describe("Lazy JSON parser", func() {
+    var parsedMap map[string]LazilyParsedJson
+
     Context("when passed an empty JSON object", func() {
-        var parsedMap map[string]LazilyParsedJson
         BeforeEach(func() {
             parsedMap = Json("{}").ParseOneLevel()
         })
-        It("Should return an empty map of strings to lazily parsed hunks of JSON", func() {
+        It("Should return an empty map", func() {
             Expect(len(parsedMap)).To(Equal(0))
+        })
+    })
+
+    Context("when passed an JSON object with one key-value pair", func() {
+        BeforeEach(func() {
+            parsedMap = Json("{\"a\":\"A\"}").ParseOneLevel()
+        })
+        It("Should return a map with one lazily parsed hunk of JSON", func() {
+            Expect(len(parsedMap)).To(Equal(1))
         })
     })
 })
