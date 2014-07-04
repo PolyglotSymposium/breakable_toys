fd5c2895c664b933e336e3bf1d8c529c861332e7
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-03 22:04:00 -0400

    After much focus, finally got reflection to work like I want it to!

--- gosoon.go
+++ gosoon.go
@@ -1,11 +1,14 @@
 package gosoon
 
 import (
-    _"strings"
-    _"reflect"
+    "reflect"
     _"fmt"
 )
 
 func Deserialize(json ParsedJson, toFill interface{}) interface{} {
+
+     mutable := reflect.ValueOf(toFill).Elem()
+     mutable.FieldByName("Phrase").SetString(json.AttributeValue("Phrase"))
+
     return toFill
 }
--- gosoon_test.go
+++ gosoon_test.go
@@ -7,18 +7,7 @@ import (
     . "github.com/onsi/gomega"
 )
 
-type Blank struct {}
-
-type OneAttribute struct {
-    GilliRocks bool
-}
-
 type OneStringAttribute struct {
-    Phrase string
-}
-
-type TwoAttributes struct {
-    KeithWouldLikeThisNameCuzCSharp int
     Phrase string
 }
 
@@ -32,69 +21,16 @@ func (self MockHasPhraseString) AttributeValue(foo string) string {
     return ""
 }
 
-type MockEmptyParsedJson struct {
-}
-
-func (self MockEmptyParsedJson) AttributeValue(foo string) string {
-    return ""
-}
-
 var _ = Describe("Gosoon", func() {
     Describe(".Deserialize", func() {
-        Context("When given an empty JSON array", func() {
-            var (
-                subject TwoAttributes
-            )
-
-            BeforeEach(func() {
-                subject = Deserialize(MockEmptyParsedJson{}, TwoAttributes{}).(TwoAttributes)
-            })
-
-            It("Should have the default value for its integer field", func() {
-                Expect(subject.KeithWouldLikeThisNameCuzCSharp).To(Equal(0))
-            })
-
-            It("Should have the default value for its string field", func() {
-                Expect(subject.Phrase).To(Equal(""))
-            })
-        })
-
-        Context("When given a JSON object, but serializing to an object with no attributes", func() {
-            var (
-                subject Blank
-            )
-
-            BeforeEach(func() {
-                subject = Deserialize(MockHasPhraseString{}, Blank{}).(Blank)
-            })
-
-            It("Should return a blank object", func() {
-                Expect(subject).To(Equal(Blank{}))
-            })
-        })
-
-        Context("When none of the properties on the JSON match the object's properties", func() {
-            var (
-                subject OneAttribute
-            )
-
-            BeforeEach(func() {
-                subject = Deserialize(MockHasPhraseString{}, OneAttribute{}).(OneAttribute)
-            })
-
-            It("Should return a blank object", func() {
-                Expect(subject).To(Equal(OneAttribute{}))
-            })
-        })
-
-
         Context("When given a JSON object with a string field (1 char), whose attribute matches the databag's", func() {
             var (
                 subject OneStringAttribute
             )
 
             BeforeEach(func() {
-                subject = Deserialize(MockHasPhraseString{}, OneStringAttribute{}).(OneStringAttribute)
+                subject = OneStringAttribute{}
+                Deserialize(MockHasPhraseString{}, &subject)
             })
 
             It("Should have the JSON value for the string field", func() {
