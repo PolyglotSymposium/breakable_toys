f5510c07f4fe7c96346c8d73b6d00b122109f5d0
Kazark <kazark@zoho.com>
2014-07-18 17:20:08 -0400

    Serialize an object with a field!

--- Serializer.go
+++ Serializer.go
@@ -1,13 +1,23 @@
 package gosoon
 
+import (
+    "reflect"
+)
+
 type JsonWriter interface {
     BeginObject()
     EndObject()
+    WriteKey(string)
+    WriteValue(kind reflect.Kind, value string)
 }
 
 func MakeSerializer(jsonWriter JsonWriter) func(interface{}) {
     return func(object interface{}) {
         jsonWriter.BeginObject()
+        if reflect.TypeOf(object).Elem().NumField() == 1 {
+            jsonWriter.WriteKey(reflect.TypeOf(object).Elem().Field(0).Name)
+            jsonWriter.WriteValue(reflect.String, "")
+        }
         jsonWriter.EndObject()
     }
 }
--- Serializer_test.go
+++ Serializer_test.go
@@ -3,6 +3,8 @@ package gosoon_test
 import (
     . "gosoon"
 
+    "reflect"
+
     . "github.com/onsi/ginkgo"
     . "github.com/onsi/gomega"
 )
@@ -13,6 +15,16 @@ type MockJsonWriter struct {
 
 func (self *MockJsonWriter) BeginObject() {
     self.json += "{"
+}
+
+func (self *MockJsonWriter) WriteKey(key string) {
+    self.json += key
+}
+
+func (self *MockJsonWriter) WriteValue(kind reflect.Kind, value string) {
+    if kind == reflect.String {
+        self.json += `""`
+    }
 }
 
 func (self *MockJsonWriter) EndObject() {
@@ -28,10 +40,18 @@ var _ = Describe("Gosoon serializer", func() {
     })
     Context("Given an object with no fields", func() {
         BeforeEach(func() {
-            serialize(struct{}{})
+            serialize(&struct{}{})
         })
         It("Should serialize it as an empty JSON object", func() {
             Expect(mock.json).To(Equal("{}"))
+        })
+    })
+    Context("Given an object with one blank string field", func() {
+        BeforeEach(func() {
+            serialize(&struct{ Foo string }{})
+        })
+        It("Should serialize it as JSON object with one field", func() {
+            Expect(mock.json).To(Equal(`{Foo""}`))
         })
     })
 })
