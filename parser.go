1fb1156227ff2e400f61544eb010250a4572da68
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-19 20:04:55 -0400

    Started reworking the parser.
    First thing, I'm starting with the negative case of bad JSON

--- parser.go
+++ parser.go
@@ -1,31 +1,13 @@
 package gosoon
 
-import "strings"
+import "errors"
 
 type ParsedJson interface {
     AttributeValue(string) string
     AttributeIsNull(string) bool
 }
 
-type LazilyParsedJson struct {
-    unparsedJson string
-}
-
-func Json(rawJson string) LazilyParsedJson {
-    return LazilyParsedJson{unparsedJson: rawJson}
-}
-
-func (self LazilyParsedJson) ParseOneLevel() map[string]LazilyParsedJson {
-    parsedMap := make(map[string]LazilyParsedJson)
-    keyvalue := strings.Split(self.unparsedJson[1:len(self.unparsedJson)-1], ":")
-    if len(keyvalue) > 1 {
-        unparsedKey := keyvalue[0]
-        parsedMap[Json(unparsedKey).ParseAsString()] = Json(keyvalue[1])
-    }
-    return parsedMap
-}
-
-func (self LazilyParsedJson) ParseAsString() string {
-    trimmed := strings.TrimSpace(self.unparsedJson)
-    return trimmed[1:len(trimmed)-1]
+func Json(rawJson string) (json ParsedJson, err error) {
+    err = errors.New("Invalid JSON given")
+    return
 }
--- parser_test.go
+++ parser_test.go
@@ -7,47 +7,21 @@ import (
     . "github.com/onsi/gomega"
 )
 
-var _ = Describe("Lazy JSON parser", func() {
-    var parsedMap map[string]LazilyParsedJson
-
-    Context("when passed an empty JSON object", func() {
-        BeforeEach(func() {
-            parsedMap = Json("{}").ParseOneLevel()
-        })
-        It("Should return an empty map", func() {
-            Expect(len(parsedMap)).To(Equal(0))
-        })
+var _ = Describe("Json", func() {
+    var (
+        toParse string
+        err error
+        parsed ParsedJson
+    )
+    BeforeEach(func() {
+        parsed, err = Json(toParse)
     })
 
-    Context("when passed an JSON object with one key-value pair", func() {
-        BeforeEach(func() {
-            parsedMap = Json("{\"a\":\"A\"}").ParseOneLevel()
-        })
-        It("Should return a map with one lazily parsed hunk of JSON", func() {
-            Expect(len(parsedMap)).To(Equal(1))
-        })
-        It("Should return a map with the key and value set appropriately", func() {
-            Expect(parsedMap["a"]).To(Equal(Json("\"A\"")))
-        })
-    })
+    Context("When given brutally invalid JSON", func() {
+        toParse = `F0oTo^Bar}{kkKkK`
 
-    Context("When parsing JSON to a string", func() {
-        var parsedString string
-        Context("When presented with a string surrounded by double-quotes", func() {
-            BeforeEach(func() {
-                parsedString = Json("\"6![]i|@nd\"").ParseAsString()
-            })
-            It("Should parse everything inside the quotes as the string", func() {
-                Expect(parsedString).To(Equal("6![]i|@nd"))
-            })
-        })
-        Context("When presented with a string surrounded by whitespace outside the quotes", func() {
-            BeforeEach(func() {
-                parsedString = Json("\t\n \" a1a \" \n\t ").ParseAsString()
-            })
-            It("Should parse everything inside the quotes as the string", func() {
-                Expect(parsedString).To(Equal(" a1a "))
-            })
+        It("Should error out", func() {
+            Expect(err).To(HaveOccurred())
         })
     })
 })
