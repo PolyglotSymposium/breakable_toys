bd2d9ce63ff178711ea40d589c03e3ef33d93494
Kazark <kazark@zoho.com>
2014-07-24 12:22:27 -0400

    Extracted loop body to method

--- Serializer.go
+++ Serializer.go
@@ -6,22 +6,24 @@ import (
 )
 
 func MakeSerializer(jsonWriter JsonWriter) func(interface{}) {
+    writeOneKeyValuePair := func(field reflect.StructField, value reflect.Value) {
+        jsonWriter.WriteCommaExceptOnFirstPass()
+        jsonWriter.WriteKey(field.Name)
+        kind := field.Type.Kind()
+        jsonWriter.WriteValue(kind, stringify(kind, value))
+    }
     return func(object interface{}) {
         jsonWriter.BeginObject()
         typeOfObject := reflect.TypeOf(object).Elem()
         valueOfObject := reflect.ValueOf(object).Elem()
         for i := 0; i < typeOfObject.NumField(); i += 1 {
-            jsonWriter.WriteCommaExceptOnFirstPass()
-            field := typeOfObject.Field(i)
-            jsonWriter.WriteKey(field.Name)
-            kind := field.Type.Kind()
-            jsonWriter.WriteValue(kind, AsJsonString(kind, valueOfObject.Field(i)))
+            writeOneKeyValuePair(typeOfObject.Field(i), valueOfObject.Field(i))
         }
         jsonWriter.EndObject()
     }
 }
 
-func AsJsonString(kind reflect.Kind, value reflect.Value) string {
+func stringify(kind reflect.Kind, value reflect.Value) string {
     if kind == reflect.String {
         return value.String()
     }
